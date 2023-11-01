938895ced122bb881acada8d11256a8a3196ea01
Wellington Facciolla Lopes <83545945+wfacciolla2@users.noreply.github.com>
2023-11-01 10:09:17 -0300

    Update controller.py

--- Controller/controller.py
+++ Controller/controller.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import csv
 def df():
-    df = pd.read_csv("C:/Users/welli/PycharmProjects/ProntoAtendimentoSB/db2.csv",
+    df = pd.read_csv("db2.csv",
                      encoding="latin-1",
                      sep=",",
                      header=0)
